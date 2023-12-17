e58fffb16ff657a5c34a655faf76f203c06877df
v1st78 <petrenkov605@gmail.com>
2023-12-17 05:56:19 +0200

    added header in App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="text-[#CDD5E0]">
+      <Header />
+      <h1>App</h1>
+      <p>App</p>
+    </div>
+  );
+}
+
+export default App;
