915cca7353946870fdac67bb9f18de0c05a19cbc
v1st78 <petrenkov605@gmail.com>
2023-12-18 01:07:26 +0200

    added Home to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#20293a] text-[#CDD5E0] font-semibold">
+    <div className="min-h-screen bg-[#20293a] font-semibold text-[#CDD5E0]">
       <Header />
+      <Home />
     </div>
   );
 }
