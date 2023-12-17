fd1077c43c89be3126ddb61eb01fb43091a9a0ad
v1st78 <petrenkov605@gmail.com>
2023-12-18 01:57:23 +0200

    added fontfamily to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Be Vietnam Pro", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
